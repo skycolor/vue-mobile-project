abd846d5aedd6933b85cbc0bd5daa09f32c48b38
hf <873276172@qq.com>
2018-07-20 20:21:24 +0800

    提交

---
+++ src/components/Base.vue
@@ -0,0 +1,19 @@
+<template>
+      <router-view></router-view>
+</template>
+<script>
+  export default {
+    name: 'baseView',
+    data () {
+      return {
+
+      }
+    },
+    components : {
+
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ src/components/page/err/err404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="err404_page">
+
+    </div>
+</template>
+<script>
+  export default {
+    name: 'err404_page',
+    data () {
+      return {
+
+      }
+    },
+    components : {
+
+    }
+  }
+</script>
+<style>
+
+</style>
